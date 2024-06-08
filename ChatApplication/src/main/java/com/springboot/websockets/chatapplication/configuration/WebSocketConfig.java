a5a2eb16eb719b80bfa0b4da91e00557a72ec2c6
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-08 09:55:48 +0300

    Working on websockets

---
+++ ChatApplication/src/main/java/com/springboot/websockets/chatapplication/configuration/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.springboot.websockets.chatapplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+}
