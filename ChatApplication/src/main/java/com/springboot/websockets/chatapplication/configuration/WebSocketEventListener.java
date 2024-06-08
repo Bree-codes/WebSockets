03704ab7545431f516c623bb75d09d715f9fe14e
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-08 10:29:19 +0300

    Working on websockets

--- ChatApplication/src/main/java/com/springboot/websockets/chatapplication/configuration/WebSocketEventListener.java
+++ ChatApplication/src/main/java/com/springboot/websockets/chatapplication/configuration/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.springboot.websockets.chatapplication.configuration;
 
+import com.springboot.websockets.chatapplication.controller.ChatMessage;
+import com.springboot.websockets.chatapplication.controller.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,10 +15,23 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent disconnectEvent){
 
-        //TODO
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString();
+
+        if(username != null){
+            log.info("User {} disconnected", username);
+            var chatMessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
 
     }
 }
