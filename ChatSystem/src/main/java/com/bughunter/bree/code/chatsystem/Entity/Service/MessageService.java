b67e4e553d033cf6bcac84a73760c88c3ea9455d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-09-19 09:49:24 +0300

    Chatting system for an application

--- ChatSystem/src/main/java/com/bughunter/bree/code/chatsystem/Entity/Service/MessageService.java
+++ ChatSystem/src/main/java/com/bughunter/bree/code/chatsystem/Entity/Service/MessageService.java
@@ -4,8 +4,12 @@ import com.bughunter.bree.code.chatsystem.Entity.Message;
 import com.bughunter.bree.code.chatsystem.Entity.Repository.MessageRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +20,14 @@ public class MessageService {
 
     public void saveMessage(Message message){
         log.info("Message saved successfully.");
+        messageRepo.save(message);
     }
 
+    public List<Message> findMessagesByTimestamp(LocalDate date){
+
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+
+        return messageRepo.findMessagesByTimestamp(startOfDay,endOfDay);
+    }
 }
